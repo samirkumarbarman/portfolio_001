001835d1c5e915b537fa5d1a8fabb797fa4ff701
Your Name <samirkumar2287@gmail.com>
2025-03-14 22:11:28 +0600

    Fix tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Projects from "./pages/Projects";
+import Projects from "./pages/Project";
 import Contact from "./pages/Contact";
 
 const App = () => {
